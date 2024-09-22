bd70e7a6cebbc7ae7c03bdf623756d9d4e6c79cc
Yeliz Tan <yeliztan2905@gmail.com>
2024-09-22 12:25:37 +0200

    v32

--- src/main/java/Runner02.java
+++ src/main/java/Runner02.java
@@ -1,6 +1,6 @@
 public class Runner02 {
     public static void main(String[] args) {
         System.out.println("Bu bir deneme projesi'dir.");
-
+        System.out.println("Bu projeye devam ediliyor");
     }
 }
